0e32e73387205f88832dfc1670424ee7ac07b1f6
Oscar Canchica <oscar@sysbytes.com.co>
2024-02-07 21:44:18 -0500

    Force HTTPS Production

--- Application/app/Providers/AppServiceProvider.php
+++ Application/app/Providers/AppServiceProvider.php
@@ -24,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        
+        if (env('APP_ENV') == 'production') {
+            URL::forceScheme('https');
+        }
+
         // Use boostrap pagination
         //Paginator::useBootstrap();
         // Shere pages data with pages layout
